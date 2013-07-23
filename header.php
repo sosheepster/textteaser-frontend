262839b40dee35619758874d011193010e902eff
Camille Zapata <camillezapata@gmail.com>
2013-07-23 11:47:57 +0800

    drafting design for textteaser

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Text Teaser</title>
+
+ 
+  <link rel="stylesheet" href="stylesheets/app.css">
+  <link rel="stylesheet" href="stylesheets/main-style.css">
+  
+  <script src="javascripts/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+
+	<header id="main-header">
+		<h1 class="site-name"><a href="#">TextTeaser</a></h1>
+		<h2 class="subtitle">Get a summary in 5 sentences!</h2>
+	</header>
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php include('header.php'); ?>
+
+	<div class="row">
+		<div class="small-12 columns">
+
+      <h3 class="section-header">Summarize here</h3>
+			<section class="content">
+				<form>
+					<textarea class="summarizer" palceholder="Paste the article here.">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu imperdiet arcu. Aenean pharetra augue cursus ligula vehicula venenatis. Morbi fringilla scelerisque massa in rhoncus. Sed sed augue et neque viverra blandit. Aenean arcu nibh, iaculis et dolor non, interdum feugiat tellus. Nulla bibendum eget nibh a ornare. Praesent eleifend commodo hendrerit. Nunc sit amet nulla a turpis tincidunt sollicitudin. Suspendisse placerat nisl sagittis, dictum est non, blandit augue. Sed fermentum, metus id pharetra faucibus, est turpis aliquet metus, non facilisis enim leo sed enim. Phasellus orci erat, malesuada et pharetra sed, tempus at eros. Morbi fermentum ut leo id imperdiet. Nullam auctor lacus vitae nisl viverra ullamcorper. Vivamus faucibus metus in vehicula sagittis. Donec at leo semper, pellentesque lectus eu, iaculis ligula. Nullam vulputate, massa hendrerit convallis hendrerit, lacus quam posuere est, et ultrices diam est eu risus.
+
+Cras commodo diam non posuere suscipit. Nullam id erat sit amet nisl volutpat auctor. Etiam id dolor rutrum, fringilla lacus at, interdum mauris. Quisque feugiat nulla quis dui ultricies feugiat. Nunc lorem purus, euismod ac nibh quis, lobortis malesuada purus. Aenean id tempus ligula, quis molestie quam. Nunc ut facilisis orci, sit amet ornare magna. Morbi at adipiscing felis, in ullamcorper urna. Vivamus sit amet quam sem. In imperdiet neque at pharetra pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed accumsan tortor vestibulum tortor bibendum fermentum. Sed a porta ante.
+
+Sed tincidunt, dolor quis tempus porta, erat arcu aliquet ipsum, ut laoreet nulla nisl in quam. Mauris elementum justo eros, eget tempor nisi facilisis ut. Pellentesque semper diam in ipsum pharetra ultricies. Aenean rutrum laoreet massa sed eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam orci leo, blandit id eleifend eu, blandit et dui. Vivamus scelerisque magna arcu, ac ultrices massa vehicula eu. Proin accumsan mi ut ante imperdiet mollis. Quisque tempus lectus iaculis sem vulputate faucibus. Phasellus interdum non ante eu eleifend. Curabitur sit amet tellus mi. Mauris pulvinar metus et fermentum tincidunt. Nunc gravida bibendum nisl eget semper.
+
+Curabitur tristique libero sit amet nulla rutrum pulvinar. In ornare tellus volutpat, volutpat nisi vitae, porta ipsum. Nunc id posuere libero. Nam at tincidunt mauris. Praesent vitae metus urna. Quisque viverra venenatis lorem, sit amet tincidunt lectus vulputate at. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Quisque eleifend scelerisque elit, eu condimentum leo convallis vel. Donec cursus gravida sem, non dictum massa faucibus non. Sed id ante et mi rhoncus tincidunt condimentum eu dolor. Mauris vitae felis justo. Maecenas blandit tortor in risus imperdiet suscipit. Donec semper tincidunt quam, nec venenatis magna fermentum ut. Duis tincidunt, quam semper dictum fermentum, risus arcu euismod metus, ut dictum metus mi vel lacus. Suspendisse quis mauris sit amet felis vulputate ultricies. Quisque vel cursus dui. Mauris hendrerit metus varius diam fermentum, sed fermentum sem lacinia. Curabitur non ultricies mi. Mauris vulputate faucibus orci, ut imperdiet quam interdum laoreet. Phasellus quis porttitor lectus, tincidunt sodales purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+					</textarea>
+
+					<input type="submit" class="button" value="Summarize">
+				</form>
+			</section>
+
+      <h3 class="section-header">In short...</h3>
+      <section class="summary content">
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+          <li>Morbi et nisi tristique, faucibus eros non, faucibus mi.</li>
+          <li>Donec tincidunt orci id scelerisque sagittis.</li>
+          <li>Ut nec justo faucibus, bibendum lectus id, ultrices enim.</li>
+          <li>Quisque sit amet neque volutpat, varius nisi sit amet, posuere tellus.</li>
+        </ul>
+      </section>
+
+
+		</div>
+	</div>
+	
+
+<?php include('footer.php'); ?>
