63fc7004f6ac07a02eff9679c016a1ad1796e3dd
Camille Zapata <camillezapata@gmail.com>
2013-07-23 13:29:20 +0800

    added favicons and header icon

--- header.php
+++ header.php
@@ -13,10 +13,24 @@
   
   <script src="javascripts/vendor/custom.modernizr.js"></script>
 
+  <link rel="icon" href="images/favicon_16x16.png" sizes="32x32" type="image/png">
+  <link rel="icon" href="images/favicon_32x32.png" sizes="32x32" type="image/png">
+  <link rel="icon" href="images/favicon_64x64.png" sizes="64x64" type="image/png">
+  <link rel="icon" href="images/favicon_128x128.png" sizes="128x128" type="image/png">
+
 </head>
 <body>
 
 	<header id="main-header">
-		<h1 class="site-name"><a href="#">TextTeaser</a></h1>
-		<h2 class="subtitle">Get a summary in 5 sentences!</h2>
+    <div class="row">
+      <div class="small-3 large-1 columns">
+        <img src="images/textteaser-icon-white.png" alt="TextTeaser" class="main-icon">
+      </div>
+      <div class="small-9 large-11 columns">
+        <div class="header-set">
+          <h1 class="site-name"><a href="#">TextTeaser</a></h1>
+          <h2 class="subtitle">Get a summary in 5 sentences!</h2>
+        </div>
+      </div>
+    </div>
 	</header>
